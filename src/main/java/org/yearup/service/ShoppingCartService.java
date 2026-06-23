c20fe13109210d53af2f95b5449fb385bebf34d5
Yumozh <juliiamozhchil@gmail.com>
2026-06-23 17:16:09 -0500

    feat: initiate shopping cart

--- src/main/java/org/yearup/service/ShoppingCartService.java
+++ src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,8 +1,13 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
+import org.yearup.models.CartItem;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.repository.ShoppingCartRepository;
+
+import java.util.List;
 
 @Service
 public class ShoppingCartService
@@ -20,8 +25,20 @@ public class ShoppingCartService
     public ShoppingCart getByUserId(int userId)
     {
         // load the user's cart rows, look up each product, and build the ShoppingCart
-        return null;
+        ShoppingCart shoppingCart = new ShoppingCart();
+        List<CartItem> cartItemList = shoppingCartRepository.findByUserId(userId);
+
+        for(CartItem item : cartItemList){
+            Product product = productService.getById(item.getProductId());
+            ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
+            shoppingCartItem.setProduct(product);
+            shoppingCartItem.setQuantity(item.getQuantity());
+            shoppingCart.add(shoppingCartItem);
+        }
+        return shoppingCart;
     }
+
+//    public void addToCart{}
 
     // add additional methods here
 }
